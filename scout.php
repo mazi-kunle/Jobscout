5d67c130b518637dd6fc5d63830fc4a0b473b151
Joy Nasinza <107001414+emilykamiti@users.noreply.github.com>
2023-03-23 18:32:59 +0300

    strapped

---
+++ scout.php
@@ -0,0 +1,48 @@
+<?php
+	session_start();
+
+	if (isset($_SESSION['emailaddress'])) {
+		header('Location: home.php');
+		exit();
+	}
+
+	if (isset($_POST['emailaddress']) && isset($_POST['password'])) {
+		$emailaddress = $_POST['emailaddress'];
+		$password = $_POST['password'];
+
+		if ($emailaddress === 'example' && $password === 'password') {
+		
+			$_SESSION['emailaddress'] = $emailaddress;
+
+			header('Location: home.php');
+			exit();
+		} else {
+			$error = 'Invalid emailaddress or password';
+		}
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Auction System Login Page</title>
+	<link rel="stylesheet" type="text/css" href="scout.css">
+</head>
+<body>
+	<div class="container">
+		<h1>Auction System Login Page</h1>
+		<?php if (isset($error)): ?>
+			<p class="error"><?php echo $error; ?></p>
+		<?php endif; ?>
+		<form action="login.php" method="post">
+			<label for="emailaddress">Email Address:</label>
+			<input type="text" id="emailaddress" name="emailaddress" required>
+
+			<label for="password">Password:</label>
+			<input type="password" id="password" name="password" required>
+
+			<input type="submit" value="Login">
+		</form>
+	</div>
+</body>
+</html>
---
+++ search.js
@@ -0,0 +1,6 @@
+Js
+function search() {
+    var location = document.getElementById("location").value;
+    var specialization = document.getElementById("specialization").value;
+    alert("You searched for " + specialization + " jobs in " + location);
+}
