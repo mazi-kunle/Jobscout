4af6f70dc9ea91aaa6f4a8978a073df0dffaf992
mazi-kunle <olakunlemichael644@gmail.com>
2023-03-23 19:44:22 +0100

    restart all

--- search.js
+++
@@ -1,6 +0,0 @@
-Js
-function search() {
-    var location = document.getElementById("location").value;
-    var specialization = document.getElementById("specialization").value;
-    alert("You searched for " + specialization + " jobs in " + location);
-}
