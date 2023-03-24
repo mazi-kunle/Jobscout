7ef4596f3b33553741e8ca85220c6280d72cbee0
Joy Nasinza <107001414+emilykamiti@users.noreply.github.com>
2023-03-24 11:02:58 +0300

    front

---
+++ frontend/ssign.php
@@ -0,0 +1,11 @@
+<?php
+if(isset($_POST['submit'])){
+	$first_name = $_POST['first_name'];
+	$last_name = $_POST['last_name'];
+	$email = $_POST['email'];
+	$contact_number = $_POST['contact_number'];
+	$password = $_POST['password'];
+	$confirm_password = $_POST['confirm_password'];
+
+}
+?>
